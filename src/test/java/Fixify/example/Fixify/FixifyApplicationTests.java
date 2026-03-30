0d544feb19d48fbc1aa687568a19ccf926300aa5
RaviPratapSingh14 <ravipratapsingh@Ravis-MacBook-Air.local>
2026-03-30 18:16:01 +0530

    Step 2: Fix test configuration by specifying SpringBootApplication class in FixifyApplicationTests

--- src/test/java/Fixify/example/Fixify/FixifyApplicationTests.java
+++ src/test/java/Fixify/example/Fixify/FixifyApplicationTests.java
@@ -1,9 +1,10 @@
 package Fixify.example.Fixify;
 
+import com.fixify.FixifyApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FixifyApplication.class)
 class FixifyApplicationTests {
 
     @Test
